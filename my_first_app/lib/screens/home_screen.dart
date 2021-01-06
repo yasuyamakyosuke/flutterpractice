ba745da57d49aff50b1ae468b9f99906efc1a1c0
Yasuyamakyosuke <toyojindai@gmail.com>
2021-01-06 17:07:43 +0900

    finish

--- my_first_app/lib/screens/home_screen.dart
+++ my_first_app/lib/screens/home_screen.dart
@@ -6,6 +6,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String _textHeader = "あなたの名前を教えてください。";
+  TextEditingController _textEditingController = TextEditingController();
+  Widget _imageWidget = Container();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,11 +17,45 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text("はじめてのアプリ"),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Text("あなたの名前を教えてください",style: TextStyle(fontSize: 10.0),)
-        ],
+      body: Container(
+        padding: EdgeInsets.all(18.0),
+        child: Column(
+          children: [
+            Center(
+                child: Text(
+              _textHeader,
+              style: TextStyle(fontSize: 20.0),
+            )),
+            TextField(
+              keyboardType: TextInputType.text,
+              style: TextStyle(fontSize: 20.0),
+              controller:
+                  _textEditingController, //TODO 入力した文字を管理するTextFieldEditing
+            ),
+            Expanded(child: _imageWidget),
+            SizedBox(
+              width: double.infinity,
+              child: RaisedButton(
+                onPressed: () => _onClick(), //ボタンを押したときの処理
+                child: Text(
+                  "押してね",
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  _onClick() {
+    setState(() {
+      //文字
+      var inputText = _textEditingController.text;
+      _textHeader = "こんにちは$inputTextさん";
+      //画像
+      _imageWidget = Image.asset("assets/images/cloud.png");
+    });
   }
 }
